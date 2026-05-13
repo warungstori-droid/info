a1174bb9d3c95da471eb814944cda8357de43ec6
aryoko <aryokotrihananto@gmail.com>
2026-05-13 14:44:09 +0700

    hood disign done

---
+++ main.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+
+  eel.init()()
+  $(".text").textillate({
+    loop: true,
+    sync: true,
+    in: {
+      effect: "bounceIn",
+    },
+    out: {
+      effect: "bounceOut",
+    },
+  });
+
+});
